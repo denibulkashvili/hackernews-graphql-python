79b72659da7b7429ebee4de91d8b997fca01877e
denibulkashvili <denibulkashvili@gmail.com>
2019-03-25 16:09:25 +0700

    Add user mutation

---
+++ users/schema.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model
+
+import graphene
+from graphene_django import DjangoObjectType
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+
+
+class CreateUser(graphene.Mutation):
+    """Defines user mutation"""
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        """Defines data to send to the server"""
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    def mutate(self, info, username, password, email):
+        """Creates a user in DB through username and email parameters"""
+        user = get_user_model()(
+            username=username,
+            email=email,
+        )
+        user.set_password(password)
+        user.save()
+
+class Mutation(graphene.ObjectType):
+    """Creates Mutation class with a filed to be resolved (create_user)"""
+    create_user = CreateUser.Field()
