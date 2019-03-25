e269ada02e4579cb875fe949dd4ab40541597283
denibulkashvili <denibulkashvili@gmail.com>
2019-03-25 15:08:17 +0700

    Add schema

--- links/schema.py
+++ links/schema.py
@@ -13,3 +13,30 @@ class Query(graphene.ObjectType):
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+
+# Define a mutation class
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    # Define data to send to the server
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    # Mutation method that creates a link in a DB 
+    # thorugh url and description params
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        return CreateLink(
+            id=link.id,
+            url=link.url,
+            description=link.description,
+        )
+
+# Create Mutation class with a field to be resolved
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
