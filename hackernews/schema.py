1b1fbc03ce7ad809b368ba7f0663a46443274437
denibulkashvili <denibulkashvili@gmail.com>
2019-03-26 23:11:55 +0700

    Fix typo

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -9,7 +9,7 @@ class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
 
 class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
-    veify_token = graphql_jwt.Verify.Field()
+    verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
