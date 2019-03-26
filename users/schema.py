16d48f2f948e515dc64e0c8d8d57370ca3502dfb
denibulkashvili <denibulkashvili@gmail.com>
2019-03-26 23:52:31 +0700

    Add query to test authentication

--- users/schema.py
+++ users/schema.py
@@ -34,9 +34,17 @@ class Mutation(graphene.ObjectType):
     """Creates Mutation class with a filed to be resolved (create_user)"""
     create_user = CreateUser.Field()
 
-class Query(graphene.ObjectType):
+class Query(graphene.AbstractType):
     """Defines query to list all users"""
     users = graphene.List(UserType)
+    me = graphene.Field(UserType)
 
     def resolve_users(self, info):
         return get_user_model().objects.all()
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Not logged in!')
+        
+        return user
